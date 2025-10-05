348bddfac4a5afda6b66939ea7bdeaa1eb33e10c
Orion-Sui <szh_dmu@163.com>
2025-10-05 14:42:06 +0800

    以太坊数量单位

---
+++ Chapter-01/010201.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// 以太坊数量单位
+
+// 1 Wei      = 10^-18ETH   =0.000000000000000001   ETH
+// 1 Gwei     = 10^-9ETH    =0.000000001            ETH 
+// 1 Finney   = 10^-3ETH    =0.001                  ETH  
+// 1 Ether    = 1ETH        
+
+// 1 Ether    = 1,000,000,000,000,000,000 Wei
+// 1 Ether    = 1,000,000,000             Gwei
+// 1 Ether    = 1,000                     Finney
